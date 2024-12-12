b8d31f525ee26ade632a4dfb26b9a11b6177f2a6
Guilherme Konzen <108848546+guikonzen1@users.noreply.github.com>
2024-12-12 11:28:08 -0400

    Update main.gd

--- scenes/main.gd
+++ scenes/main.gd
@@ -5,12 +5,12 @@ extends Node2D
 @export var powerup: PackedScene
 @export var cactus: PackedScene
 @export var playtime: int = 30
-var time = playtime
-var level := 1
-var score := 0
-var playing := false
-var hit_count := 0
-var powerup_cont = 0
+var time :int= playtime
+var level :int= 1
+var score :int= 0
+var playing :bool= false
+var hit_count :int= 0
+var powerup_cont :int= 0
 signal resetcount
 @onready var viewport = get_viewport().get_visible_rect().size
 @onready var game_time: Timer = $GameTime
@@ -33,6 +33,7 @@ func _process(_delta: float) -> void:
 		$PowerupTimer.wait_time = randf_range(5, 10)
 		$PowerupTimer.start()
 
+# Start button
 func _on_hud_game_start() -> void:
 	player.visible = true
 	player.start_game()
@@ -55,6 +56,7 @@ func game_over():
 	$PowerupTimer.stop()
 	$GameOverSound.play()
 
+# Chama isso sempre que clica no start
 func new_game():
 	player.position = viewport/2
 	score = 0
@@ -69,12 +71,12 @@ func new_game():
 func spawn_coins():
 	$NewLevelSound.play()
 	powerup_cont += 1
-	if powerup_cont >= 5:
+	if powerup_cont >= 5: # Se ficar 5 levels sem pegar powerup, spawna 1
 		spawn_powerup()
 	for i in level + 3 + difficult:
 		var c := coins.instantiate()
 		add_child(c)
-		c.position = Vector2(randi_range(10, viewport.x - 10), randi_range(10, viewport.y - 10))
+		c.position = Vector2(randi_range(5, viewport.x - 5), randi_range(5, viewport.y - 5))
 
 func _on_player_pickup(type: Variant) -> void:
 	match type:
@@ -96,22 +98,31 @@ func _on_powerup_timer_timeout() -> void:
 func spawn_powerup():
 	var p := powerup.instantiate()
 	add_child(p)
-	p.position = Vector2(randi_range(10, viewport.x - 10), randi_range(10, viewport.y - 10))
+	p.position = Vector2(randi_range(0, viewport.x), randi_range(0, viewport.y))
+
 
 func _on_resetcount() -> void:
 	powerup_cont = 0
 
+
+# Spawnando os cactus aleatoriamente e a quantidade com base na dificuldade
 func respawn_cactus():
 	get_tree().call_group("obstacles", "queue_free")
-	for i in 2 + difficult:
+	for i in 1 + difficult:
 		var c = cactus.instantiate()
 		$Node2D.add_child(c)
 		c.position = Vector2(randi_range(10, viewport.x - 10), randi_range(10, viewport.y - 10))
 
 func _on_hud_minus() -> void:
-	difficult = hud.difficult
+	difficult = clampi(difficult - 1, 1, 10)
+	# mesma coisa dessas duas linhas:
+	# difficult -= 1
+	# difficult = clampi(difficult, 1, 10)
+	hud.update_difficult(difficult)
 	respawn_cactus()
 
 func _on_hud_plus() -> void:
+	difficult = clampi(difficult + 1, 1, 10)
+	hud.update_difficult(difficult)
 	difficult = hud.difficult
 	respawn_cactus()

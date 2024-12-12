cb7292184ec7df5ae7314ac4739899015720cb09
Guilherme Konzen <108848546+guikonzen1@users.noreply.github.com>
2024-12-12 11:29:12 -0400

    Update cactus.gd

--- scenes/player/cactus.gd
+++ scenes/player/cactus.gd
@@ -1,6 +1,7 @@
 extends Area2D
 var screensize = Vector2(480, 720)
 
+#Evitando que o cacto fique em cima de outro cacto e da safe area
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("obstacles") or area.is_in_group("safe"):
 		self.position = Vector2(randf_range(0, screensize.x), randf_range(0, screensize.y))
